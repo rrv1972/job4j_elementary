4e02346683edfe24ddbe3f4d109e314be42d2ebf
rrv1972 <4352415@gmail.com>
2023-06-20 16:22:31 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+    System.out.println("Petr Arsentev");
+            System.out.println("06.01.2019"); }
+ }
+
--- src/main/java/ru/job4j/info.java
+++ src/main/java/ru/job4j/info.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
-
-public class info {
+public class Info {
     public static void main(String[] args) {
 System.out.println("20.03.2023");
     }
