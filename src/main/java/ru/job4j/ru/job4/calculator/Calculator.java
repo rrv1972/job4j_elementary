2055d74de9eb80132bf5bf5e855e904396713a06
rrv1972 <4352415@gmail.com>
2023-06-22 11:22:53 +0300

    3. Арифметические операции

---
+++ src/main/java/ru/job4j/ru/job4/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package ru.job4j.ru.job4.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePusTwo = one + two;
+        System.out.println(onePusTwo);
+    }
+}
