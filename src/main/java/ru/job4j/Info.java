b893bcc8358348a925f9ca8d0ec931303d182d87
rrv1972 <4352415@gmail.com>
2023-06-22 18:16:17 +0300

    метод

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,5 +1,5 @@
 package ru.job4j;
-public class Info {
+/*public class Info {
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
@@ -10,5 +10,19 @@ public class Info {
         Info.plus(4, 2);
         Info.plus(3, 5);
     }
+}/*
+
+ */
+public class Info {
+    public static void hello(String name) {
+                System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args)  {
+        String name = "Petr Arsentev";
+        Info.hello(name);
+
+    }
+
 }
 
