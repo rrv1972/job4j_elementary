3fd44a9fb270b475b4acb1646251caf3ddc3147f
rrv1972 <4352415@gmail.com>
2023-06-26 12:27:00 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,21 +2,28 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static float rubtoEuro(float value) {
-        float result = value / 70;
+    public static double rubtoEuro(double value) {
+        double result = value / 70;
         return result;
     }
 
-    public static float rubtoDollar(float value) {
-        float result = value / 60;
+    public static double rubtoDollar(double value) {
+        double result = value / 60;
         return result;
     }
 
     public static void main(String[] args) {
+        double in = 140;
+        double expectedeuro  = 2;
+        double expecteddollar = 2.3333333;
+        double euro = rubtoEuro(in); /*вызов подпрог с возвратом значения перевода руб в евро */
+        boolean passedeuro = expectedeuro == euro;
+        double dollar = rubtoDollar(in); /*вызов подпрограмы с возвратом значения перевода руб в доллар*/
+        boolean passeddollar = dollar == expecteddollar;
+        System.out.println(" 140 рублей равно 2 евро это " + passedeuro);
+        System.out.println(" 140 рулей равно " + dollar + " доллара это " + passeddollar);
 
-        float euro = rubtoEuro(140); /*вызов подпрог с возвратом значения перевода руб в евро */
-        float dollar = rubtoDollar(160); /*вызов подпрограмы с возвратом значения перевода руб в доллар*/
         System.out.println("140 rub = " + euro + " euro");
-        System.out.println("160 rub = " + dollar + " dollar");
+        System.out.println("140 rub = " + dollar + " dollar");
     }
 }
