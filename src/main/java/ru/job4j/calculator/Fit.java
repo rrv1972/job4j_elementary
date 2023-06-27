1bde38b9df85ce4f46c71009f7c4ad3bc3213794
rrv1972 <4352415@gmail.com>
2023-06-27 11:10:58 +0300

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,22 +2,22 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double weithMen(double height) {
+    public static double weightMan(double height) {
       double  rsl = (height - 100) * 1.15;
         return rsl;
 
     }
 
-    public static double weithWomen(double height) {
+    public static double weightWoman(double height) {
         double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
 public static void main(String[] args) {
     double height = 187;
-    double weithmen = weithMen(height);
-    double weithwomen = weithWomen(height);
-    System.out.println("При росте 187 cm вес мужчины должен быть " + weithmen);
-    System.out.println("При росте 187 cm вес женщины должен быть " + weithwomen);
+    double weightman = weightMan(height);
+    double weightwomen = weightWoman(height);
+    System.out.println("При росте 187 cm вес мужчины должен быть " + weightman);
+    System.out.println("При росте 187 cm вес женщины должен быть " + weightwomen);
 }
 }
